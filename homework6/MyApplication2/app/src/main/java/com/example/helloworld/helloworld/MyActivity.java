8c0580ff02b9257a4b924e1a81526dc8013f9e2b
alex9249 <alex9249@hotmail.com>
2014-11-24 15:52:39 -0500

    Homework 6
    I kept getting an assertion error when trying to implement a fragment so
    i was unable to do part 3.

---
+++ homework6/MyApplication2/app/src/main/java/com/example/helloworld/helloworld/MyActivity.java
@@ -0,0 +1,51 @@
+package com.example.helloworld.helloworld;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+import com.parse.Parse;
+import com.parse.PushService;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Parse.initialize(this, "APPLICATION ID", "CLIENT KEY");
+        // Also in this method, specify a default Activity to handle push notifications
+        PushService.setDefaultPushCallback(this, MyActivity.class);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+    public void broadcastIntent(View view)
+    {
+        Intent intent = new Intent();
+        intent.setAction("com.tutorialspoint.CUSTOM_INTENT");
+        sendBroadcast(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
